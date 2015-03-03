8a899462d6b985b312584e47d91c06d6deb8ef00
DELGADO Stephane <stephane.delgado@epita.fr>
2015-03-04 00:00:13 +0100

    Correct this totally buggy bin_emulator script

--- bin_emulator.sh
+++ bin_emulator.sh
@@ -1,40 +1,54 @@
 #! /bin/bash
 
-function print_usage {
-    echo -e "Usage:\t$0 [OPTION] [FILE]...\n\nOPTION\n------\n"
+function print_usage
+{
+    echo -e "Usage:\t$0 [OPTION] [FILE]...\n"
+
+    echo -e "Without options, FILE will be throw to The bin (~/.Trash)\n"
+
+    echo -e "OPTION\n------\n"
     echo -e "-s | --status :\n\tSee the current bin content"
-    echo -e "-e | --empty :\n\tEmpty the Trash"
+    echo -e "--clean :\n\tEmpty the Trash"
     echo -e "-h | --help :\n\tPrint this usage\n"
 
-    echo -e "Without options, FILE will be throw to The bin (~/.Trash)"
+    echo -e "Call bin without options and files is equivalent to use 'bin --status'"
 }
-function bin_status {
-    $TRASH_NUMBER=`ls -l | wc -l`
+
+function bin_status
+{
+    TRASH_DESCRIPTION=$(tree ~/.Trash | tail -n 1)
+
+    echo "Bin status"
+    echo -e "----------\n"
 
     ls -l ~/.Trash
-    echo -e "\nThe bin contains $TRASH_NUMBER trash files."
+    echo -e "\nBin content: $TRASH_DESCRIPTION."
 }
 
-function bin_throw {
-    if [ $# -eq 0 ]; then
-        echo Error: No files target... Do nothing.
-    fi
+function bin_throw
+{
+    nbr_cpy=0
 
-    for i in `seq 2 $#`; do
-        mv -t ~/.Trash
-        echo Move `eval echo \${$i}` to Trash.
+    for i in `seq 1 $#`; do
+        ARG_i=$(eval echo \${$i})
+
+        mv --backup=numbered -t ~/.Trash $ARG_i && \
+            echo Move $ARG_i to Trash. && \
+            nbr_cpy=$(($nbr_cpy + 1))
     done
 
-    echo -e "\nSummary: Move $(($# - 1)) files to Trash."
+    echo -e "\nSummary: Move $nbr_cpy files to Trash."
 }
 
-function bin_emulator {
-    if [ $1 = '--status' ] || [ $1 = '-s']; then
+function bin_emulator
+{
+    if [ $# -eq 0 ] || [ $1 = '-s' ] || [ $1 = '--status' ]; then
         bin_status
-    elif [ $1 = '-e' ] || [ $1 = '--empty' ]; then
-        rm -rf ~/.Trash/*
     elif [ $1 = '-h' ] || [ $1 = '--help' ]; then
         print_usage
+    elif [ $1 = '--clean' ]; then
+        echo Clean the bin
+        rm -rf ~/.Trash/*
     else
         bin_throw $@
     fi
